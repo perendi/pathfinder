806f500b341ca30c81862acc99ce4ba3c4192430
Daniel Perendi <dmperendi1@sheffield.ac.uk>
2020-04-06 15:23:16 +0100

    dijkstra implemented

---
+++ src/Algorithms/dijkstra.js
@@ -0,0 +1,40 @@
+export function dijkstra(grid, startNode, finishNode) {
+  const visitedNodesInOrder = [];
+  nodes[startNode].distance = 0;
+  const unvisitedNodes = getAllNodes(grid);
+  while (!!unvisitedNodes.length) {
+    sortNodesByDistance(unvisitedNodes);
+    const closestNode = unvisitedNodes.shift();
+    // HANDLE WALLS LATER
+    // while(currentNode.status == "wall" && unvisitedNodes.length){
+    //     currentNode = getClosestNode(nodes, unvisitedNodes);
+    // }
+    // HANDE IMPOSSIBLE LATER
+    // if(closestNode.distance === Infinity) return false;
+    // ANIMATE LATER
+    // nodesToAnimate.push(closestNode;)
+    closestNode.isVisited = true;
+    if (closestNode === finishNode) return visitedNodesInOrder;
+    updateNeighbours(closestNode, grid);
+  }
+}
+
+function sortNodesByDistance(unvisitedNodes) {
+  unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+}
+
+function updateNeighbours(node, grid) {
+  const neighbours = getNeighbours(node, grid);
+  for (const neighbour of neighbours) {
+    neighbour.distance = node.distance + 1;
+  }
+}
+
+function getNeighbours(node, grid) {
+  const neighbours = [];
+  const { col, row } = node;
+  if (row > 0) neighbours.push(grid[row - 1][col]);
+  if (row < grid.length - 1) neighbours.push(grid[row + 1][col]);
+  if (col > 0) neighbours.push(grid[row][col - 1]);
+  if (col < grid[0].length - 1) neighbours.push(grid[row][col + 1]);
+}
--- src/PathfindingVisualiser/PathfindingVisualiser.jsx
+++ src/PathfindingVisualiser/PathfindingVisualiser.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import Node from "./Node/Node";
+import { dijkstra, getNodesInShortestPathOrder } from '../Algorithms/dijkstra';
 
 import "./PathfindingVisualiser.css";
+
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 15;
+const FINISH_NODE_COL = 30;
 
 export default class PathfindingVisualiser extends Component {
   constructor(props) {
@@ -12,43 +18,71 @@ export default class PathfindingVisualiser extends Component {
   }
 
   componentDidMount() {
-    const nodes = [];
-    for (let row = 0; row < 20; row++) {
-      const currentRow = [];
+    const grid = this.getInitialGrid();
+    this.setState({ grid });
+  }
+  
+  // handleMouseDown(row, col) {
+  //   const newGrid = this.getNewGridWithWallToggled(this.state.grid, row, col);
+  //   this.setState({ grid: newGrid, mouseIsPressed: true });
+  // }
 
-      for (let col = 0; col < 50; col++) {
-        const currentNode = {
-          col,
-          row,
-          isStart: row === 10 && col === 5,
-          isFinish: row === 15 && col === 45,
-        };
-        currentRow.push(currentNode);
-      }
+  // handleMouseEnter(row, col) {
+  //   if (!this.state.mouseIsPressed) return;
+  //   const newGrid = this.getNewGridWithWallToggled(this.state.grid, row, col);
+  //   this.setState({ grid: newGrid });
+  // }
 
-      nodes.push(currentRow);
+  // handleMouseUp() {
+  //   this.setState({ mouseIsPressed: false });
+  // }
+
+  animateDijkstra(visitedNodesInOrder) {
+    for (const node of visitedNodesInOrder) {
+      const newGrid = this.state.grid.slice();
+      const newNode = {
+        ...node,
+        isVisited: true,
+      };
+      newGrid[node.row][node.col] = newNode;
+      setTimeout(() => {
+        this.setState({ grid: newGrid });
+      }, 100);
     }
+  }
 
-    this.setState({ nodes });
+  visualiseDijkstra() {
+    const { grid } = this.state;
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+    //const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    this.animateDijkstra(visitedNodesInOrder);
   }
 
   render() {
-    const { nodes } = this.state;
-    console.log(nodes);
+    const { grid} = this.state;
 
     return (
+      <button onClick={()=>this.visualiseDijkstra()}>Visualise Dijkstra's Algorithm</button>
       <div className="grid">
         {nodes.map((row, rowIdx) => {
           return (
             <div key={rowIdx}>
               {row.map((node, nodeIdx) => {
-                const { isStart, isFinish } = node;
-                console.log(isStart);
+                const {row, col, isStart, isFinish, isWall } = node;
                 return (
                   <Node
                     key={nodeIdx}
+                    row={col}
+                    col={col}
                     isStart={isStart}
                     isFinish={isFinish}
+                    isWall={isWall}
+                    mouseIsPressed={mouseIsPressed}
+                    onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                    onMouseEnter={(row, col) => this.handleMouseEnter(row, col)}
+                    onMouseUp={() => this.handleMouseUp()}
                   ></Node>
                 );
               })}
@@ -59,3 +93,40 @@ export default class PathfindingVisualiser extends Component {
     );
   }
 }
+const getInitialGrid = () => {
+  const grid = [];
+  for (let row = 0; row < 20; row++) {
+    const currentRow = [];
+
+    for (let col = 0; col < 50; col++) {
+      currentRow.push(this.createNode(col, row));
+    }
+
+    grid.push(currentRow);
+  }
+  return grid;
+};
+
+const createNode = (col, row) => {
+  return {
+    col,
+    row,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
+  };
+};
+
+const getNewGridWithWallToggled = (grid, row, col) => {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isWall: !node.isWall,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
+};
